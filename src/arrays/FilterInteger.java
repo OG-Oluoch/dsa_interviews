34a49e55789c5110264c858b742bec9fd68a3135
OG-Oluoch <makopiyo45@gmail.com>
2026-05-12 08:34:22 +0300

    working on arrays

--- src/arrays/FilterInteger.java
+++ src/arrays/FilterInteger.java
@@ -10,20 +10,56 @@ output = {9,3}
 
 public class FilterInteger {
 
-    void printArray(int[] arr){
+   static void printArray(int[] arr){
         int n = arr.length;
 
         for(int i =0;i<n;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+" ");
         }
+    }
+
+    static int[] removeEven(int[] demoArray){
+        int oddCount = 0;
+
+        for(int i=0;i<demoArray.length;i++){
+
+            if(demoArray[i]%2!=0){
+                oddCount++;
+            }
+
+        }
+
+        int [] result = new int[oddCount];
+
+        int idx =0;
+
+        for(int i=0;i<demoArray.length;i++){
+
+            if(demoArray[i]%2!=0){
+
+                result[idx]=demoArray[i];
+
+                idx++;
+            }
+
+        }
+
+        return result;
+
     }
 
     public static void main(String[] args) {
 
-        int [] sampleArray = {4,9,3,7,1,6,5};
+        int [] sampleArray = {4,9,3,8,7,1,6,5}; // 9,3,7,1,5
 
-        FilterInteger filterInteger = new FilterInteger();
-        filterInteger.printArray(sampleArray);
+       printArray(sampleArray);
+        System.out.println();
+
+        System.out.println("After removing even integers");
+
+      int[] finalResult = removeEven(sampleArray);
+
+      printArray(finalResult);
 
     }
 }
